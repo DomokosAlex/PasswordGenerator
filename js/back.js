0c25e278f5ac5522518ca673232ff34e3955cd69
Alex <domokosalex01@gmail.com>
2025-04-10 20:46:01 +0200

    Transition works

--- js/back.js
+++ js/back.js
@@ -1,4 +1,16 @@
-
-function func(){
-    document.body.style.backgroundImage = 'url("./images/red.svg")';
-}
+function red() {
+    const overlay = document.getElementById('overlay');
+    overlay.style.backgroundColor = 'rgba(255, 0, 0, 0.55)'; 
+  }
+  
+  
+  function blue() {
+    const overlay = document.getElementById('overlay');
+    overlay.style.backgroundColor = 'rgba(0, 0, 255, 0.55)'; 
+  }
+  
+  
+  function green() {
+    const overlay = document.getElementById('overlay');
+    overlay.style.backgroundColor = 'rgba(0, 255, 0, 0.55)'; 
+  }
