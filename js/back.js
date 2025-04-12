7929404a4b17e543bb70d1343084076b3bf6694c
unknown <maxth686@gmail.com>
2025-04-12 22:55:41 +0200

    Changeable Background + Fancier

--- js/back.js
+++ js/back.js
@@ -1,16 +1,21 @@
-function red() {
-    const overlay = document.getElementById('overlay');
-    overlay.style.backgroundColor = 'rgba(255, 0, 0, 0.55)'; 
-  }
-  
-  
-  function blue() {
-    const overlay = document.getElementById('overlay');
-    overlay.style.backgroundColor = 'rgba(0, 0, 255, 0.55)'; 
-  }
-  
-  
-  function green() {
-    const overlay = document.getElementById('overlay');
-    overlay.style.backgroundColor = 'rgba(0, 255, 0, 0.55)'; 
-  }
+const overlay = document.getElementById('overlay');
+const darkoverlay = document.getElementById("darkoverlay");
+
+const hatter = document.getElementById("Color");
+const sotetites = document.getElementById("Sot");
+const elmosodas = document.getElementById("Elmos");
+
+hatter.addEventListener("input", function () {
+  overlay.style.backgroundColor = hatter.value;
+});
+
+
+sotetites.addEventListener("input", function () {
+
+  darkoverlay.style.backgroundColor = "rgba(0, 0, 0," + sotetites.value/10 + ")";
+});
+
+
+elmosodas.addEventListener("input", function(){
+darkoverlay.style.backdropFilter = "blur(" + elmosodas.value+ "px";
+});
