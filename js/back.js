60cbdb10b4b04ff535eff09ad261195c3e642ae1
unknown <maxth686@gmail.com>
2025-04-18 20:10:53 +0200

    JS soon 2

--- js/back.js
+++ js/back.js
@@ -6,42 +6,66 @@ const sotetites = document.getElementById("Sot");
 const elmosodas = document.getElementById("Elmos");
 
 hatter.addEventListener("input", function () {
-  
+
   overlay.style.backgroundColor = hatter.value;
 });
 
-elmosodas.addEventListener("input", function(){
-  overlay.style.backdropFilter = "blur(" + elmosodas.value+ "px)";
-  });
+elmosodas.addEventListener("input", function () {
+  overlay.style.backdropFilter = "blur(" + elmosodas.value + "px)";
+});
 
 sotetites.addEventListener("input", function () {
 
-  darkoverlay.style.backgroundColor = "rgba(0, 0, 0," + sotetites.value/10 + ")";
+  darkoverlay.style.backgroundColor = "rgba(0, 0, 0," + sotetites.value / 10 + ")";
 });
-
-
-
 
 
 const nagy = document.getElementById("nagy");
 const kis = document.getElementById("kis");
 const szam = document.getElementById("szam");
 const spec = document.getElementById("spec");
+const cont1 = document.getElementById("atl1");
+
 
 
 
 const gomb = document.getElementById("general");
 
-gomb.addEventListener("click", function(e){
+gomb.addEventListener("click", function (e) {
 
-  e.preventDefault();
-  let string = "";
+  let pickfrom = [];
+ 
+  if(nagy.checked == true){
 
-  if(nagy.checked){
-    let number = 65;
-let letter = String.fromCharCode(number);
-console.log(letter); // Outputs: "A"
+    pickfrom.push(1);
   }
+
+
+  if(kis.checked == true){
+
+    pickfrom.push(2);
+  }
+
+
+  if(szam.checked == true){
+
+    pickfrom.push(3);
+  }
+
+
+  if(spec.checked == true){
+
+    pickfrom.push(4);
+  }
+
+
+
+  if(cont1.checked == true){
+
+  }else{
+    
+  }
+
 
 
 
