516707f6ff685124ce1ee3f0f85a1556592c3d2b
unknown <maxth686@gmail.com>
2025-04-21 17:34:06 +0200

    Works, is better

--- js/back.js
+++ js/back.js
@@ -1,53 +1,105 @@
-//checkboxok
-const nagy = document.getElementById("nagy");
-const kis = document.getElementById("kis");
-const szam = document.getElementById("szam");
-const spec = document.getElementById("spec");
-const cont1 = document.getElementById("atl1");
+//checkbox
+const nagybetuk = document.getElementById("nagybetuk");
+const kisbetuk = document.getElementById("kisbetuk");
+const szamok = document.getElementById("szamok");
+const specialkarakter = document.getElementById("specialkarakter");
+const min1 = document.getElementById("min1");
 
-//hossza, hova irja ki
-const hossz = document.getElementById("hossz");
+//passlength
+const passlen = document.getElementById("passlen");
+
+//hova irja ki
 var kiir = document.getElementById("kiir");
 
 //generaljon
 const gomb = document.getElementById("general");
 
-//mibol valasszon, szam nincs kozottuk mert folosleges tarolni, csak generalok egy szamot, nem kell index
-var nagybetu = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var kisbetu = "abcdefghijklmnopqrstuvwxyz";
-var special = "!@#$%^&*()_+[]{}";
 
-
-
-
+//keverje fel a karaktereket, given an array it mixes the characters
 function Shuffle(pass_slice) {
 
-  let shuffles = Math.floor(Math.random() * hossz.max) + 1;
 
-  for (let i = 0; i <= shuffles; i++) {
+  for (let i = 0; i <= pass_slice.length - 1; i++) {
 
     let ind1 = Math.floor(Math.random() * pass_slice.length);
     let ind2 = Math.floor(Math.random() * pass_slice.length);
 
+    /*
     let temp = pass_slice[ind1];
     pass_slice[ind1] = pass_slice[ind2];
     pass_slice[ind2] = temp;
+    */
+    //3rd variable not neccesary, nem kotelezo
+    [pass_slice[ind1], pass_slice[ind2]] = [pass_slice[ind2], pass_slice[ind1]];
 
   }
 
-  pass_slice = pass_slice.join("");
-
+  //return the array
   return pass_slice;
 
 }
 
 
-function PasswordGen() {
+function RandomCharacter(set) {
+
+  //pick a random index from the character set 
+  return set[Math.floor(Math.random() * set.length)];
+
+}
+
+
+function PasswordGen(check) {
+
   const pass = [];
+  const mandatory = [];
 
-  
+  //stores checks
+  const checks = [nagybetuk.checked, kisbetuk.checked, szamok.checked, specialkarakter.checked];
 
+  //count how many are checked
+  var count = 0;
 
+  const sets = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", "!@#$%^&*()_+[]{}"];
+
+  checks.forEach(e => {
+    if (e) {
+      count++
+    }
+  });
+
+  //if that category is checked it will be as an index, which character set to use
+  var category_counter = 0;
+
+  checks.forEach(e => {
+
+    if (e) {
+
+      for (let i = 0; i < passlen.value; i++) {
+        pass.push(RandomCharacter(sets[category_counter]));
+      }
+      if (check) {
+        mandatory.push(RandomCharacter(sets[category_counter]));
+
+      }
+      remains = passlen.length - pass.length;
+
+    }
+
+    category_counter++
+
+  });
+  const beforecheck = Shuffle(pass).slice(0, pass.length / count);
+
+  //if there are mandatory characters, add them
+  if (check) {
+    for (let i = 0; i < mandatory.length; i++) {
+      beforecheck[i] = mandatory[i];
+
+    }
+  }
+
+  //shuffle again because of new characters, turn to string and return
+  return Shuffle(beforecheck).join("");
 
 }
 
@@ -57,15 +109,13 @@ gomb.addEventListener("click", function (e) {
   //alapertelmezett mukodes alljon meg
   e.preventDefault();
 
-  //hogyha legalabb 1-nek lennie kell per kategoria
-  if (cont1.checked) {
-
-    //csak LEHETSEGES hogy benne van
+  if (!nagybetuk.checked && !kisbetuk.checked && !szamok.checked && !specialkarakter.checked) {
+    kiir.value = "Select 1 category at minimum";
   } else {
-
-    NemKotelezo();
-
+    //Password is given to the form element 
+    kiir.value = PasswordGen(min1.checked);
   }
+
 
 });
 
